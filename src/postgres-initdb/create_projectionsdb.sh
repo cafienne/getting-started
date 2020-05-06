b19848e41361745db3454dd4acef9eaa1962e0a2
Olger Warnier <olger@spectare.nl>
2020-05-06 06:29:04 +0200

    Changed cafienne postgres to use a single database instance.

---
+++ src/postgres-initdb/create_projectionsdb.sh
@@ -0,0 +1,6 @@
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "cafienne-query";
+    GRANT ALL PRIVILEGES ON DATABASE "cafienne-query" TO postgresuser;
+EOSQL
